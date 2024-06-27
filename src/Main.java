874a43234e180e8e7dbeee1cd4f7e29d38cf2abd
ZakariaEljazouli2024 <zakazakaria@gmail.com>
2024-06-27 17:51:20 -0400

    rename projet test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-/*New Projet*/
+/*New Projet v2*/
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
